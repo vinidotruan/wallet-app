4ad696f3b85f88ce4004e1de6c2e366b800c9425
Ruan Vinícius <contatoruanvinicius@gmail.com>
2020-02-16 22:49:28 -0300

    - Troca footerbar de posição

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Routes from './routes';
-import FooterBar from './components/FooterBar';
 import './styles.css';
 
 function App() {
   return (
     <div className="App">
       <Routes />
-      <FooterBar />
     </div>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import List from './pages/solutions/list';
 import Login from './pages/login';
+import FooterBar from './components/FooterBar';
+
+
 
 const Routes = () => (
     <BrowserRouter>
@@ -9,7 +12,10 @@ const Routes = () => (
             <Route exact path="/" component={Login}/>
             <Route path="/solutions" component={List}/>
         </Switch>
+        
+        <FooterBar/>
     </BrowserRouter>
+
 )
 
 export default Routes;
